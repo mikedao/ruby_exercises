fb4db7f3ac57a61d04d97cc98fcf3e309c2843f7
Michael Dao <michael.dao@gmail.com>
2014-11-01 19:31:16 -0600

    Exercise 1 Complete

---
+++ objects-and-methods/exercise-1/lib/bag.rb
@@ -0,0 +1,27 @@
+require_relative 'candy'
+class Bag
+  attr_reader :candies
+
+  def initialize
+    @candies = []
+  end
+
+  def empty?
+    @candies.empty?
+  end
+
+  def count
+    @candies.count
+  end
+
+  def <<(candy)
+    @candies << candy
+  end
+
+  def contains?(type)
+    candies.any? do |candy|
+      candy.type == type
+    end
+  end
+
+end
